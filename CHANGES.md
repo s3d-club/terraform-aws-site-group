ef4d3feb7e8251219c5c5ccb7c99cc9db132899d
Mark Farnsworth <farnsworth2008@gmail.com>
2022-11-18 04:01:19 +0000

    0.1.46 Updated `aws` provider
    - Updated `aws` provider

--- CHANGES.md
+++ CHANGES.md
@@ -4,12 +4,12 @@ This file describes the project history as it relates to tagged versions.
 ## [0.0.0](.) [0.1.31](.)
 - Created early versions
 
-## [0.1.32](.) to [0.1.44](.)
+## [0.1.32](.) to [0.1.45](.)
 - Added `aws_s3_bucket_logging` resource
 - Added `aws_s3_bucket_server_side_encryption_configuration` resource
 - Fixed sort order for terraform code
 - Improved descriptions of variables and outputs
 - Updated modules and providers
 
-## [0.1.45](.)
-- Reformated `terraform.tf`
+## [0.1.46](.)
+- Updated `aws` provider
--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ locals {
 
 module "ec2_work" {
   count  = var.enable_ec2 ? 1 : 0
-  source = "github.com/s3d-club/terraform-aws-ec2?ref=v0.1.33"
+  source = "github.com/s3d-club/terraform-aws-ec2?ref=v0.1.34"
 
   domain        = var.domain
   egress_cidr6s = var.egress_cidr6s
@@ -49,7 +49,7 @@ module "ec2_work" {
 
 module "ecr" {
   for_each = toset(var.ecrs)
-  source   = "github.com/s3d-club/terraform-aws-ecr?ref=v0.1.21"
+  source   = "github.com/s3d-club/terraform-aws-ecr?ref=v0.1.22"
 
   kms_key_arn = local.kms_key_arn
   name_prefix = each.key
@@ -67,7 +67,7 @@ module "name" {
 # tfsec:ignore:aws-ec2-no-public-ingress-sgr
 module "sg_ingress_open" {
   count  = var.cidrs == null ? 0 : 1
-  source = "github.com/s3d-club/terraform-aws-sg_ingress_open?ref=v0.1.17"
+  source = "github.com/s3d-club/terraform-aws-sg_ingress_open?ref=v0.1.18"
 
   cidrs       = var.cidrs
   cidr6s      = var.cidr6s
